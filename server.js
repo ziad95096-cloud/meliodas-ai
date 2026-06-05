d6612af5e4ae2814be28c7816bd62fdcec185347
ziad95096-cloud <ziad95096@gmail.com>
2026-06-05 21:47:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`⚡ Meliodas AI running on port ${PORT}`);
-});
+export default app;
